f35a32a0bdc13936904ac1bbdba4234c6763db98
jennifer <jennifer.analis@gmail.com>
2023-04-22 12:03:30 -0400

    arreglos s3 y s4

---
+++ 3-S3/evaluacion/evaluacion.py
@@ -0,0 +1,9 @@
+import math
+
+#declaracion de variable
+y = 81 #asignando valor a la variable
+
+raiz = math.sqrt(y)
+
+#impresion de la variable raiz
+print(f"La raiz cuadradada de {y} es : {raiz}")
---
+++ 4.S4/rebound/rebound.py
@@ -0,0 +1,19 @@
+"""
+Requerimos crear una variable con el número 8, una con el número 10.5, y una con la palabra “ejercicio”. 
+Luego, crear un set que contendrá cada una de las variables que creamos, y será asignado a una variable.
+A continuación, crearemos una lista que contendrá el set creado anteriormente, 
+y una variable con el valor lógico False. 
+Esta lista la asignaremos a una variable que luego imprimiremos en pantalla.
+"""
+
+variable_uno = 8
+variable_dos = 10.5
+variable_tres = 'ejercicio'
+
+un_set = {variable_uno, variable_dos, variable_tres}
+
+a = False
+
+una_lista = [un_set, a]
+
+print(una_lista)
